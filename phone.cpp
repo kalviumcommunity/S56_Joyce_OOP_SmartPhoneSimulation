34b9621a19afcdd6f3df6d269195a1b0520dbf48
Joyce Ubale <joyceubale@Joyces-MacBook-Air.local>
2024-08-13 22:50:01 +0530

    Array

--- phone.cpp
+++ phone.cpp
@@ -1,105 +1,103 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-class Screen{
-    private:
+class Screen {
+private:
     int bright;
     int charge;
-    
-    public:
-    Screen(): bright(0),charge(0){}
-    
-    void setBrightness(const int newBright){
-        bright=newBright;
+
+public:
+    Screen(): bright(0), charge(0) {}
+
+    void setBrightness(const int newBright) {
+        bright = newBright;
     }
-    
-    void getBrightness(){
-        if(bright<10){
-            cout<<"lower the brightness"<<endl;
-        }
-        else{
-            cout<< bright<<endl;
+
+    void getBrightness() {
+        if (bright < 10) {
+            cout << "lower the brightness" << endl;
+        } else {
+            cout << bright << endl;
         }
     }
-    
-    void setCharging(const int newCharge){
-        charge=newCharge;
+
+    void setCharging(const int newCharge) {
+        charge = newCharge;
     }
-    
-    void getCharging(){
-        if(charge<15){
-            cout<<"charge your phone"<<endl;
-        }
-        else{
-            cout<<charge<<endl;
+
+    void getCharging() {
+        if (charge < 15) {
+            cout << "charge your phone" << endl;
+        } else {
+            cout << charge << endl;
         }
     }
 };
 
-class App{
-    private:
+class App {
+private:
     int productive;
     int nonProductive;
     int time;
 
-    public:
-    App(): productive(0),nonProductive(0),time(0){}
+public:
+    App(): productive(0), nonProductive(0), time(0) {}
 
-    void setProd(const int newProductive, int newNonProductive){
-        productive=newProductive;
-        nonProductive=newNonProductive;
+    void setProd(const int newProductive, int newNonProductive) {
+        productive = newProductive;
+        nonProductive = newNonProductive;
     }
 
-    void getProd(){
-        if(productive>nonProductive){
-            cout<<"The productivity of your apps is more"<<endl;
+    void getProd() {
+        if (productive > nonProductive) {
+            cout << "The productivity of your apps is more" << endl;
+        } else {
+            cout << "Non-Productive apps are more uninstall few!" << endl;
         }
-        else{
-            cout<<"Non-Productive apps are more uninstall few!"<<endl;
-        }
     }
 
-    void setTime(const int newTime){
-        time=newTime;
+    void setTime(const int newTime) {
+        time = newTime;
     }
-    int getTime(){
+
+    int getTime() {
         return time;
     }
 };
 
 int main() {
-  // Enter your code below
-  Screen objScreen;
-  cout<<"Brightness level:";
-  int bright;
-  cin>>bright;
-  objScreen.setBrightness(bright);
-  objScreen.getBrightness();
-  
-  cout<<"Charging level:";
-  int charge;
-  cin>>charge;
-  objScreen.setCharging(charge);
-  objScreen.getCharging();
+    // Array of Screen objects
+    Screen screens[2];  // Array of 2 Screen objects
+    
+    for(int i = 0; i < 2; i++) {
+        cout << "Screen " << i + 1 << " - Brightness level: ";
+        int bright;
+        cin >> bright;
+        screens[i].setBrightness(bright);
+        screens[i].getBrightness();
+        
+        cout << "Screen " << i + 1 << " - Charging level: ";
+        int charge;
+        cin >> charge;
+        screens[i].setCharging(charge);
+        screens[i].getCharging();
+    }
 
-  cout<<"No. of Productive apps:";
-  int productive;
-  cin>>productive;
-  cout<<"No. of NonProductive apps:";
-  int nonProductive;
-  cin>>nonProductive;
-  App objApp;
-  objApp.setProd(productive,nonProductive);
-  objApp.getProd();
+    App objApp;
+    cout << "No. of Productive apps: ";
+    int productive;
+    cin >> productive;
+    cout << "No. of Non-Productive apps: ";
+    int nonProductive;
+    cin >> nonProductive;
+    objApp.setProd(productive, nonProductive);
+    objApp.getProd();
   
-  cout<<"Screen Time:";
-  int time;
-  cin>>time;
-  objApp.setTime(time);
-  objApp.getTime();
-  return 0;
+    cout << "Screen Time: ";
+    int time;
+    cin >> time;
+    objApp.setTime(time);
+    cout << "Total screen time: " << objApp.getTime() << " minutes" << endl;
+
+    return 0;
 }
