7168f2daaf3edc619cedcefbd97a9f8ff6c1aff6
Joyce Ubale <joyceubale@Joyces-Air.mshome.net>
2024-09-23 10:51:46 +0530

    constructor destructor

--- phone.cpp
+++ phone.cpp
@@ -1,105 +1,117 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-class Screen{
-    private:
+class Screen {
+private:
     int bright;
     int charge;
-    
-    public:
-    Screen(): bright(0),charge(0){}
-    
-    void setBrightness(const int newBright){
-        bright=newBright;
+
+public:
+    Screen(int initBright, int initCharge) : bright(initBright), charge(initCharge) {
+        cout << "Screen object created with Brightness: " << bright << " and Charge: " << charge << "%" << endl;
     }
-    
-    void getBrightness(){
-        if(bright<10){
-            cout<<"lower the brightness"<<endl;
-        }
-        else{
-            cout<< bright<<endl;
+
+    ~Screen() {
+        cout << "Screen object destroyed" << endl;
+    }
+
+    void setBrightness(const int newBright) {
+        bright = newBright;
+    }
+
+    void getBrightness() {
+        if (bright < 10) {
+            cout << "Lower the brightness" << endl;
+        } else {
+            cout << "Brightness is: " << bright << endl;
         }
     }
-    
-    void setCharging(const int newCharge){
-        charge=newCharge;
+
+    void setCharging(const int newCharge) {
+        charge = newCharge;
     }
-    
-    void getCharging(){
-        if(charge<15){
-            cout<<"charge your phone"<<endl;
-        }
-        else{
-            cout<<charge<<endl;
+
+    void getCharging() {
+        if (charge < 15) {
+            cout << "Charge your phone" << endl;
+        } else {
+            cout << "Charge level is: " << charge << "%" << endl;
         }
     }
 };
 
-class App{
-    private:
+class App {
+private:
     int productive;
     int nonProductive;
     int time;
 
-    public:
-    App(): productive(0),nonProductive(0),time(0){}
+public:
 
-    void setProd(const int newProductive, int newNonProductive){
-        productive=newProductive;
-        nonProductive=newNonProductive;
+    App(int initProductive, int initNonProductive, int initTime) 
+        : productive(initProductive), nonProductive(initNonProductive), time(initTime) {
+        cout << "App object created with Productive apps: " << productive 
+             << ", Non-Productive apps: " << nonProductive 
+             << ", and Screen Time: " << time << " hours" << endl;
     }
 
-    void getProd(){
-        if(productive>nonProductive){
-            cout<<"The productivity of your apps is more"<<endl;
+    ~App() {
+        cout << "App object destroyed" << endl;
+    }
+
+    void setProd(const int newProductive, const int newNonProductive) {
+        productive = newProductive;
+        nonProductive = newNonProductive;
+    }
+
+    void getProd() {
+        if (productive > nonProductive) {
+            cout << "The productivity of your apps is more" << endl;
+        } else {
+            cout << "Non-Productive apps are more, uninstall a few!" << endl;
         }
-        else{
-            cout<<"Non-Productive apps are more uninstall few!"<<endl;
-        }
     }
 
-    void setTime(const int newTime){
-        time=newTime;
+    void setTime(const int newTime) {
+        time = newTime;
     }
-    int getTime(){
+
+    int getTime() {
         return time;
+    }
+
+    void displayScreenTime() {
+        cout << "Screen time is: " << time << " hours." << endl;
     }
 };
 
 int main() {
-  // Enter your code below
-  Screen objScreen;
-  cout<<"Brightness level:";
-  int bright;
-  cin>>bright;
-  objScreen.setBrightness(bright);
-  objScreen.getBrightness();
-  
-  cout<<"Charging level:";
-  int charge;
-  cin>>charge;
-  objScreen.setCharging(charge);
-  objScreen.getCharging();
+    // Create Screen object using constructor
+    int brightness, charge;
+    cout << "Enter brightness level: ";
+    cin >> brightness;
+    cout << "Enter charging level: ";
+    cin >> charge;
+    Screen objScreen(brightness, charge);
+    
+    // Screen operations
+    objScreen.getBrightness();
+    objScreen.getCharging();
 
-  cout<<"No. of Productive apps:";
-  int productive;
-  cin>>productive;
-  cout<<"No. of NonProductive apps:";
-  int nonProductive;
-  cin>>nonProductive;
-  App objApp;
-  objApp.setProd(productive,nonProductive);
-  objApp.getProd();
-  
-  cout<<"Screen Time:";
-  int time;
-  cin>>time;
-  objApp.setTime(time);
-  objApp.getTime();
-  return 0;
+    // Create App object using constructor
+    int productive, nonProductive, time;
+    cout << "Enter the number of productive apps: ";
+    cin >> productive;
+    cout << "Enter the number of non-productive apps: ";
+    cin >> nonProductive;
+    cout << "Enter screen time (in hours): ";
+    cin >> time;
+    App objApp(productive, nonProductive, time);
+    
+    // App operations
+    objApp.getProd();
+    objApp.displayScreenTime();
+
+    // Main function ends, destructors will be called automatically
+    return 0;
 }
